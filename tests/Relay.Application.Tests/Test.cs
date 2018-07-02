d79a339912deea320d33fb155feccb9ed6b19895
Palmetto <jelinski89@gmail.com>
2018-07-02 18:50:14 +0200

    created scaffolding

---
+++ tests/Relay.Application.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace Relay.Application.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void FrameworkTest()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
