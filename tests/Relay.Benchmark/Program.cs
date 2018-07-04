9fc7690a7512aacbce338de9cb72bb78229e5bb8
Palmetto <jelinski89@gmail.com>
2018-07-04 15:58:16 +0200

    added benchmark

---
+++ tests/Relay.Benchmark/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Relay.Benchmark
+{
+    using System;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Running;
+    using Application;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RelayBenchmark>();
+            Console.ReadKey();
+        }
+    }
+
+    public class StubSubscriber : ISubscriber
+    {
+        public Task<bool> ReceiveMsg(Message msg) => Task.FromResult(true);
+    }
+
+    public class RelayBenchmark
+    {
+        private Relay _relay;
+        private Message _message;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _relay = new Relay();
+            _relay.AddSubscriber(new StubSubscriber());
+            _message = new Message("benchmark");
+        }
+
+        [Benchmark]
+        public async Task BroadcastSingleMessage()
+        {
+            await _relay.Broadcast(_message);
+        }
+    }
+}
