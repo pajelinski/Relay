f3c8b7d89795d00a60f25d0ad898f790d763a923
Palmetto <jelinski89@gmail.com>
2018-07-05 16:21:14 +0200

    added console demo

--- src/Relay/Program.cs
+++ src/Relay/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Relay
+﻿using System.Diagnostics;
+using System.Linq;
+using Relay.Application;
+
+namespace Relay
 {
     using System;
 
@@ -6,7 +10,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var relay = new Application.Relay();
+
+            var subscribers = Enumerable.Range(0, 9).Select(id => new Subscriber(id));
+
+            foreach (var subscriber in subscribers)
+            {
+                relay.AddSubscriber(subscriber);
+            }
+
+            Console.WriteLine("To start broadcast press any key");
+
+            Console.ReadKey();
+
+            Console.WriteLine("Started broadcast...");
+            var run = true;
+
+            for (var messageCount = 0; run; messageCount++)
+            {
+                relay.Broadcast(new Message($"test message {messageCount}"));
+                run = WasQPressed();
+            }
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+
+        private static bool WasQPressed()
+        {
+            Console.WriteLine();
+            Console.WriteLine("To end broadcast press q");
+            Console.WriteLine("To continue press any key");
+            Console.WriteLine();
+                var consoleKeyInfo = Console.ReadKey(false);
+                return consoleKeyInfo.Key != ConsoleKey.Q;
         }
     }
 }
---
+++ src/Relay/Subscriber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Relay.Application;
+
+namespace Relay
+{
+    public class Subscriber : ISubscriber
+    {
+        private readonly int _id;
+
+        public Subscriber(int id)
+        {
+            _id = id;
+        }
+
+        public Task<bool> ReceiveMsg(Message msg)
+        {
+            Console.WriteLine($"Subscriber: {_id} receivedMessage: {msg.Body}");
+            return Task.FromResult(true);
+        }
+    }
+}
